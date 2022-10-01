5866afe81f7d6a9d49f2a8cc7dbee2c36cecc8d1
Anton <zaharov.28097411@gmail.com>
2022-10-01 19:07:36 +0500

    Initial Commit

---
+++ kvaga-ios/kvaga_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  kvaga_iosApp.swift
+//  kvaga-ios
+//
+//  Created by Антон Захаров on 01.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct kvaga_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
